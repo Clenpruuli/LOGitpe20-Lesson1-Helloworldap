a6603598b7da3c1d6e74fc878c9d32be7dabdd21
Opilane TTHK <opilane@tthk.ee>
2020-09-01 13:20:30 +0300

    make

---
+++ Helloworld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Console.WriteLine("Hello World!");
+            //this is a commentary
+            //console.WriteLine("Hello Clen!");
+            /* 1. The program ask the users name
+             * 2. The user enters their name
+             * 3. The program greets the user by their name*/
+            Console.WriteLine("What`s you name?");
+            string Name;
+            //read the input from the user
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello, " + Name);
+        }
+    }
+}
